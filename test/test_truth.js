11f1fe55840174a256bf094cd5d1b9a3c9338fcb
Anton Siluev <burning2007@ya.ru>
2018-12-17 23:57:02 +0300

    add failing dummy tes

---
+++ test/test_truth.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const { assert, expect } = require('chai')
+
+describe('The Truth', function() {
+  it('should be thruthly thrue, definitelly', function() {
+    expect(true).to.false
+  })
+})
